4215b332d17a2645c1f675cc06009d6e392be2e8
ClaytonStealth <claytonses@yahoo.com>
2023-04-10 16:03:16 -0400

    fixed log in routing and error checks

--- routes/users/controller/userController.js
+++ routes/users/controller/userController.js
@@ -4,21 +4,21 @@ const { errorHandler } = require("./userHelper");
 module.exports = {
   login: async (req, res) => {
     try {
-      let newUser = await new User({
-        username: req.body.username,
-        password: req.body.password,
-      });
+      let foundUser = await User.findOne({ username: req.body.username });
+      if (!foundUser) {
+        throw {
+          status: 404,
+          message: "Username does not exists",
+        };
+      }
       // console.log(newUser);
-      let savedUser = await newUser.save();
       res.status(200).json({
         message: "POST request from controller",
-        userObj: savedUser,
+        userObj: foundUser,
       });
     } catch (e) {
-      console.log(e);
-      res.status(409).json({
-        message: e,
-      });
+      let errorMessage = await errorHandler(e);
+      res.status(errorMessage.status).json({message: errorMessage.message});
     }
   },
   register: async (req, res) => {
@@ -28,7 +28,7 @@ module.exports = {
         throw {
           status: 409,
           message: "Username already exists",
-        }
+        };
       }
       let newUser = await new User({
         username: req.body.username,
